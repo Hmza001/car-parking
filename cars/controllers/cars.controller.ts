4b7458af548556989db6c6059f78cf7eb95f1978
Hamza <writehmza@gmail.com>
2020-08-13 13:40:33 -0700

    controller change

--- cars/controllers/cars.controller.ts
+++ cars/controllers/cars.controller.ts
@@ -4,7 +4,9 @@ import carsService from "../services/cars.service";
 class CarsController {
   async listCars(req: express.Request, res: express.Response) {
     const { startDate, endDate } = req.query;
+
     const users = await carsService.list(startDate, endDate);
+    
     res.status(200).send(users);
   }
 }
